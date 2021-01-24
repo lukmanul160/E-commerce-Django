6645e969dda4ce163076a9533fe555550e135dab
lukmanul160 <lukmanul160@gmail.com>
2021-01-24 20:41:25 +0700

    2 commit cart,checkout

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'ecommerce.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,3 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+MEDIA_ROOT = 'static/images'
+MEDIA_URL = '/images/'
